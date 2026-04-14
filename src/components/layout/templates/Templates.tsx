1817a434e1ffd80b24a906495c7b7b49d15f4832
Zohar Peretz <zohar@gmail.com>
2026-04-14 22:11:28 +0300

    ..

--- src/components/layout/templates/Templates.tsx
+++ src/components/layout/templates/Templates.tsx
@@ -3,16 +3,22 @@ import './Templates.css'
 import GovCitiesGrid from './GovCitiesGrid'
 import GovCitySelect from './GovCitySelect'
 import NumberSelect from './NumberSelect'
+import ReduxCounter from './ReduxCounter'
+import ReduxCounterMirror from './ReduxCounterMirror'
 
 export default function Templates() {
-  const [selectedNumber, setSelectedNumber] = useState(1)
-  const [selectedCity, setSelectedCity] = useState('')
+    const [selectedNumber, setSelectedNumber] = useState(1)
+    const [selectedCity, setSelectedCity] = useState('')
 
-  return (
-    <div className='Templates'>
-      <NumberSelect value={selectedNumber} onChange={setSelectedNumber} />
-      <GovCitySelect value={selectedCity} onChange={setSelectedCity} />
-      <GovCitiesGrid />
-    </div>
-  )
+    return (
+        <div className='Templates'>
+            <NumberSelect value={selectedNumber} onChange={setSelectedNumber} />
+            <GovCitySelect value={selectedCity} onChange={setSelectedCity} />
+            <div>
+                <ReduxCounter />
+                <ReduxCounterMirror />
+            </div>
+            <GovCitiesGrid />
+        </div>
+    )
 }
